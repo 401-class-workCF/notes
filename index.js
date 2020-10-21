0ea878efa96596bfe2465a2e6924611b689e46de
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-10-20 23:51:08 -0700

    having trouble with db

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const mongoose = require('mongoose');
 
 const Input = require('./lib/input');
 const Notes = require('./lib/notes');
+const url = 'mongodb://localhost:27017/notes';
+// const url = 'mongodb://127.0.0.1:27017/notes';
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.once('open', (_) => {
+  console.log('Database connected:', url);
+});
+
+db.on('error', (err) => {
+  console.error('connection error:', err);
+});
 
 const input = new Input();
 const notes = new Notes(input);
